b605272e0274577e6f862c03d8b710d520f82c44
2441294779@qq.com <46741song>
2018-09-08 12:19:09 +0800

    首页完成

--- app/src/main/java/scrollview/com/cn/myproject/entity/CodeBean.java
+++ app/src/main/java/scrollview/com/cn/myproject/entity/CodeBean.java
@@ -1,19 +1,29 @@
 package scrollview.com.cn.myproject.entity;
 
 import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Transient;
+
+import java.io.Serializable;
 
 @Entity
-public class CodeBean {
+public class CodeBean implements Serializable {
     @Id
     private Long id;
     private String content;
     private String stage;
+    private static final long serialVersionUID = 1L;
 
+    @Generated(hash = 1587485775)
     public CodeBean(Long id, String content, String stage) {
         this.id = id;
         this.content = content;
         this.stage = stage;
+    }
+
+    @Generated(hash = 544591002)
+    public CodeBean() {
     }
 
     public Long getId() {
